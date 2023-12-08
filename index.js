e69e38b987ebd6cd941eefadba783a6544d08c6c
sachin kumar <sachin.kumar@pivotroots.com>
2023-12-08 14:30:41 +0530

    f-commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sharp = require("sharp");
 
-sharp("./img/Screenshot from 2023-09-04 17-25-38.png")
+sharp("./img/Screenshot from 2023-09-13 19-23-56.png")
   .rotate()
   .resize(200)
   .jpeg({ mozjpeg: true })
